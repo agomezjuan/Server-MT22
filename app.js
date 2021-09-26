cc7f9c16258e255b1b34d98cef7ba9bcf78328da
Alberto Gomez Juan <agomezjuan@hotmail.com>
2021-09-26 23:38:21 +0200

    Implementación de asignación automática del puerto

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
+// Asignación manual del puerto
+/*
 app.listen(3000, function () {
   console.log("El servidor escucha por el puerto 3000");
+});
+*/
+
+// Asignación del puerto automaticamente con nodemon
+app.set("puerto", process.env.PORT || 5000);
+app.listen(app.get("puerto"), function () {
+  console.log("App de ejemplo escuchando por el puerto " + app.get("puerto"));
 });
